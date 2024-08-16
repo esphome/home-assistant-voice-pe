083ec926d866bb0d6fea57b50e623709d071b6cc
Kevin Ahrendt <kahrendt@gmail.com>
2024-08-16 08:41:28 -0400

    dac settings adjusting the clock

--- esphome/components/nabu/nabu_media_player.h
+++ esphome/components/nabu/nabu_media_player.h
@@ -64,6 +64,9 @@ class NabuMediaPlayer : public Component,
   void set_sample_rate(uint32_t sample_rate) { this->sample_rate_ = sample_rate; }
 
   void set_volume_increment(float volume_increment) { this->volume_increment_ = volume_increment; }
+  
+  void reconfigure_dac_new_settings();
+  void reconfigure_dac_old_settings();
 
  protected:
   // Receives commands from HA or from the voice assistant component
