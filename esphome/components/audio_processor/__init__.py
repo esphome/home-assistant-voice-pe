106c1eb9f9f8c2bf9e68a01d815f0b2f48809a1b
Keith Burzinski <kbx81x@gmail.com>
2024-08-25 23:43:07 -0500

    Remove #define
    Co-authored-by: Jesse Hills <3060199+jesserockz@users.noreply.github.com>

--- esphome/components/audio_processor/__init__.py
+++ esphome/components/audio_processor/__init__.py
@@ -9,5 +9,4 @@ audio_processor_ns = cg.esphome_ns.namespace("audio_processor")
 
 @coroutine_with_priority(100.0)
 async def to_code(config):
-    cg.add_define("USE_AUDIO_PROCESSOR")
-    # cg.add_global(audio_processor_ns.using)
+    pass
