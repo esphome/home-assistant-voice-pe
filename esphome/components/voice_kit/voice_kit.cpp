348cd6f3909fbd3a514116e939b9be5cbae76d88
Keith Burzinski <kbx81x@gmail.com>
2024-09-09 18:06:26 -0500

    Implement automatic XMOS firmware updating (#68)
    Co-authored-by: Jesse Hills <3060199+jesserockz@users.noreply.github.com>

--- esphome/components/voice_kit/voice_kit.cpp
+++ esphome/components/voice_kit/voice_kit.cpp
@@ -1,7 +1,11 @@
 #include "voice_kit.h"
 
+#include "esphome/core/application.h"
+#include "esphome/core/component.h"
 #include "esphome/core/defines.h"
 #include "esphome/core/log.h"
+
+#include <cinttypes>
 
 namespace esphome {
 namespace voice_kit {
@@ -21,18 +25,207 @@ void VoiceKit::setup() {
     if (!this->dfu_get_version_()) {
       ESP_LOGE(TAG, "Communication with Voice Kit failed");
       this->mark_failed();
+    } else if (!this->versions_match_() && this->firmware_bin_is_valid_()) {
+      ESP_LOGW(TAG, "XMOS firmware version is incorrect -- updating...");
+      this->start_dfu_update();
     }
   });
 }
 
 void VoiceKit::dump_config() {
   ESP_LOGCONFIG(TAG, "Voice Kit:");
-  LOG_PIN("  Reset Pin: ", this->reset_pin_);
   LOG_I2C_DEVICE(this);
+  LOG_PIN("  Reset Pin: ", this->reset_pin_);
+  if (this->firmware_version_major_ || this->firmware_version_minor_ || this->firmware_version_patch_) {
+    ESP_LOGCONFIG(TAG, "  XMOS firmware version: %u.%u.%u", this->firmware_version_major_,
+                  this->firmware_version_minor_, this->firmware_version_patch_);
+  }
+}
+
+void VoiceKit::loop() {
+  switch (this->dfu_update_status_) {
+    case UPDATE_IN_PROGRESS:
+    case UPDATE_REBOOT_PENDING:
+    case UPDATE_VERIFY_NEW_VERSION:
+      this->dfu_update_status_ = this->dfu_update_send_block_();
+      break;
+
+    case UPDATE_COMMUNICATION_ERROR:
+    case UPDATE_TIMEOUT:
+    case UPDATE_FAILED:
+    case UPDATE_BAD_STATE:
+      this->mark_failed();
+      break;
+
+    default:
+      break;
+  }
+}
+
+void VoiceKit::start_dfu_update() {
+  if (this->firmware_bin_ == nullptr || !this->firmware_bin_length_) {
+    ESP_LOGE(TAG, "Firmware invalid");
+    return;
+  }
+
+  ESP_LOGI(TAG, "Starting update from %u.%u.%u...", this->firmware_version_major_, this->firmware_version_minor_,
+           this->firmware_version_patch_);
+
+  if (!this->dfu_set_alternate_()) {
+    ESP_LOGE(TAG, "Set alternate request failed");
+    this->dfu_update_status_ = UPDATE_COMMUNICATION_ERROR;
+    return;
+  }
+
+  this->bytes_written_ = 0;
+  this->last_progress_ = 0;
+  this->last_ready_ = millis();
+  this->update_start_time_ = millis();
+  this->dfu_update_status_ = this->dfu_update_send_block_();
+}
+
+VoiceKitUpdaterStatus VoiceKit::dfu_update_send_block_() {
+  i2c::ErrorCode error_code = i2c::NO_ERROR;
+  uint8_t dfu_dnload_req[MAX_XFER + 6] = {240, 1, 130,  // resid, cmd_id, payload length,
+                                          0, 0};        // additional payload length (set below)
+                                                        // followed by payload data with null terminator
+  if (millis() > this->last_ready_ + DFU_TIMEOUT_MS) {
+    ESP_LOGE(TAG, "DFU timed out");
+    return UPDATE_TIMEOUT;
+  }
+
+  if (this->bytes_written_ < this->firmware_bin_length_) {
+    if (!this->dfu_check_if_ready_()) {
+      return UPDATE_IN_PROGRESS;
+    }
+
+    // read a maximum of MAX_XFER bytes into buffer (real read size is returned)
+    auto bufsize = this->load_buf_(&dfu_dnload_req[5], MAX_XFER, this->bytes_written_);
+    ESP_LOGVV(TAG, "size = %u, bytes written = %u, bufsize = %u", this->firmware_bin_length_, this->bytes_written_,
+              bufsize);
+
+    if (bufsize > 0 && bufsize <= MAX_XFER) {
+      // write bytes to XMOS
+      dfu_dnload_req[3] = (uint8_t) bufsize;
+      error_code = this->write(dfu_dnload_req, sizeof(dfu_dnload_req) - 1);
+      if (error_code != i2c::ERROR_OK) {
+        ESP_LOGE(TAG, "DFU download request failed");
+        return UPDATE_COMMUNICATION_ERROR;
+      }
+      this->bytes_written_ += bufsize;
+    }
+
+    uint32_t now = millis();
+    if ((now - this->last_progress_ > 1000) or (this->bytes_written_ == this->firmware_bin_length_)) {
+      this->last_progress_ = now;
+      float percentage = this->bytes_written_ * 100.0f / this->firmware_bin_length_;
+      ESP_LOGD(TAG, "Progress: %0.1f%%", percentage);
+    }
+    return UPDATE_IN_PROGRESS;
+  } else {  // writing the main payload is done; work out what to do next
+    switch (this->dfu_update_status_) {
+      case UPDATE_IN_PROGRESS:
+        if (!this->dfu_check_if_ready_()) {
+          return UPDATE_IN_PROGRESS;
+        }
+        memset(&dfu_dnload_req[3], 0, MAX_XFER + 2);
+        // send empty download request to conclude DFU download
+        error_code = this->write(dfu_dnload_req, sizeof(dfu_dnload_req) - 1);
+        if (error_code != i2c::ERROR_OK) {
+          ESP_LOGE(TAG, "Final DFU download request failed");
+          return UPDATE_COMMUNICATION_ERROR;
+        }
+        return UPDATE_REBOOT_PENDING;
+
+      case UPDATE_REBOOT_PENDING:
+        if (!this->dfu_check_if_ready_()) {
+          return UPDATE_REBOOT_PENDING;
+        }
+        ESP_LOGI(TAG, "Done in %.0f seconds -- rebooting XMOS SoC...",
+                 float(millis() - this->update_start_time_) / 1000);
+        if (!this->dfu_reboot_()) {
+          return UPDATE_COMMUNICATION_ERROR;
+        }
+        this->last_progress_ = millis();
+        return UPDATE_VERIFY_NEW_VERSION;
+
+      case UPDATE_VERIFY_NEW_VERSION:
+        if (millis() > this->last_progress_ + 200) {
+          this->last_progress_ = millis();
+          if (!this->dfu_get_version_()) {
+            return UPDATE_VERIFY_NEW_VERSION;
+          }
+        } else {
+          return UPDATE_VERIFY_NEW_VERSION;
+        }
+        if (!this->versions_match_()) {
+          ESP_LOGE(TAG, "Update failed");
+          return UPDATE_FAILED;
+        } else {
+          ESP_LOGI(TAG, "Update complete");
+        }
+        return UPDATE_OK;
+
+      default:
+        ESP_LOGW(TAG, "Unknown state");
+        return UPDATE_BAD_STATE;
+    }
+  }
+  return UPDATE_BAD_STATE;
+}
+
+uint32_t VoiceKit::load_buf_(uint8_t *buf, const uint8_t max_len, const uint32_t offset) {
+  if (offset > this->firmware_bin_length_) {
+    ESP_LOGE(TAG, "Invalid offset");
+    return 0;
+  }
+
+  uint32_t buf_len = this->firmware_bin_length_ - offset;
+  if (buf_len > max_len) {
+    buf_len = max_len;
+  }
+
+  for (uint8_t i = 0; i < max_len; i++) {
+    buf[i] = this->firmware_bin_[offset + i];
+  }
+  return buf_len;
+}
+
+bool VoiceKit::version_read_() {
+  return this->firmware_version_major_ || this->firmware_version_minor_ || this->firmware_version_patch_;
+}
+
+bool VoiceKit::versions_match_() {
+  return this->firmware_bin_version_major_ == this->firmware_version_major_ &&
+         this->firmware_bin_version_minor_ == this->firmware_version_minor_ &&
+         this->firmware_bin_version_patch_ == this->firmware_version_patch_;
+}
+
+bool VoiceKit::dfu_get_status_() {
+  const uint8_t status_req[] = {DFU_CONTROLLER_SERVICER_RESID,
+                                DFU_CONTROLLER_SERVICER_RESID_DFU_GETSTATUS | DFU_COMMAND_READ_BIT, 6};
+  uint8_t status_resp[6];
+
+  auto error_code = this->write(status_req, sizeof(status_req));
+  if (error_code != i2c::ERROR_OK) {
+    ESP_LOGE(TAG, "Request status failed");
+    return false;
+  }
+
+  error_code = this->read(status_resp, sizeof(status_resp));
+  if (error_code != i2c::ERROR_OK || status_resp[0] != CTRL_DONE) {
+    ESP_LOGE(TAG, "Read status failed");
+    return false;
+  }
+  this->status_last_read_ms_ = millis();
+  this->dfu_status_next_req_delay_ = encode_uint24(status_resp[4], status_resp[3], status_resp[2]);
+  this->dfu_state_ = status_resp[5];
+  this->dfu_status_ = status_resp[1];
+  ESP_LOGVV(TAG, "status_resp: %u %u - %ums", status_resp[1], status_resp[5], this->dfu_status_next_req_delay_);
+  return true;
 }
 
 bool VoiceKit::dfu_get_version_() {
-  const uint8_t DFU_CONTROLLER_SERVICER_RESID_DFU_GETVERSION = 88;
   const uint8_t version_req[] = {DFU_CONTROLLER_SERVICER_RESID,
                                  DFU_CONTROLLER_SERVICER_RESID_DFU_GETVERSION | DFU_COMMAND_READ_BIT, 4};
   uint8_t version_resp[4];
@@ -50,7 +243,51 @@ bool VoiceKit::dfu_get_version_() {
   }
 
   ESP_LOGI(TAG, "DFU version: %u.%u.%u", version_resp[1], version_resp[2], version_resp[3]);
+  this->firmware_version_major_ = version_resp[1];
+  this->firmware_version_minor_ = version_resp[2];
+  this->firmware_version_patch_ = version_resp[3];
+
   return true;
+}
+
+bool VoiceKit::dfu_reboot_() {
+  const uint8_t reboot_req[] = {DFU_CONTROLLER_SERVICER_RESID, DFU_CONTROLLER_SERVICER_RESID_DFU_REBOOT, 1};
+
+  auto error_code = this->write(reboot_req, 4);
+  if (error_code != i2c::ERROR_OK) {
+    ESP_LOGE(TAG, "Reboot request failed");
+    return false;
+  }
+  return true;
+}
+
+bool VoiceKit::dfu_set_alternate_() {
+  const uint8_t setalternate_req[] = {DFU_CONTROLLER_SERVICER_RESID, DFU_CONTROLLER_SERVICER_RESID_DFU_SETALTERNATE, 1,
+                                      DFU_INT_ALTERNATE_UPGRADE};  // resid, cmd_id, payload length, payload data
+
+  auto error_code = this->write(setalternate_req, sizeof(setalternate_req));
+  if (error_code != i2c::ERROR_OK) {
+    ESP_LOGE(TAG, "SetAlternate request failed");
+    return false;
+  }
+  return true;
+}
+
+bool VoiceKit::dfu_check_if_ready_() {
+  if (millis() >= this->status_last_read_ms_ + this->dfu_status_next_req_delay_) {
+    if (!this->dfu_get_status_()) {
+      return false;
+    }
+    ESP_LOGVV(TAG, "DFU state: %u, status: %u, delay: %" PRIu32, this->dfu_state_, this->dfu_status_,
+              this->dfu_status_next_req_delay_);
+
+    if ((this->dfu_state_ == DFU_INT_DFU_IDLE) || (this->dfu_state_ == DFU_INT_DFU_DNLOAD_IDLE) ||
+        (this->dfu_state_ == DFU_INT_DFU_MANIFEST_WAIT_RESET)) {
+      this->last_ready_ = millis();
+      return true;
+    }
+  }
+  return false;
 }
 
 }  // namespace voice_kit
--- esphome/components/voice_kit/voice_kit.h
+++ esphome/components/voice_kit/voice_kit.h
@@ -13,7 +13,9 @@ namespace voice_kit {
 static const uint8_t DFU_CONTROLLER_SERVICER_RESID = 240;
 static const uint8_t CONFIGURATION_SERVICER_RESID = 241;
 static const uint8_t DFU_COMMAND_READ_BIT = 0x80;
+
 static const uint16_t DFU_TIMEOUT_MS = 1000;
+static const uint16_t MAX_XFER = 128;  // maximum number of bytes we can transfer per block
 
 enum TransportProtocolReturnCode : uint8_t {
   CTRL_DONE = 0,
@@ -21,18 +23,131 @@ enum TransportProtocolReturnCode : uint8_t {
   CTRL_INVALID = 3,
 };
 
+enum VoiceKitUpdaterStatus : uint8_t {
+  UPDATE_OK,
+  UPDATE_COMMUNICATION_ERROR,
+  UPDATE_READ_VERSION_ERROR,
+  UPDATE_TIMEOUT,
+  UPDATE_FAILED,
+  UPDATE_BAD_STATE,
+  UPDATE_IN_PROGRESS,
+  UPDATE_REBOOT_PENDING,
+  UPDATE_VERIFY_NEW_VERSION,
+};
+
+// DFU enums from https://github.com/xmos/sln_voice/blob/develop/examples/ffva/src/dfu_int/dfu_state_machine.h
+
+enum DfuIntAltSetting : uint8_t {
+  DFU_INT_ALTERNATE_FACTORY,
+  DFU_INT_ALTERNATE_UPGRADE,
+};
+
+enum DfuIntState : uint8_t {
+  DFU_INT_APP_IDLE,    // unused
+  DFU_INT_APP_DETACH,  // unused
+  DFU_INT_DFU_IDLE,
+  DFU_INT_DFU_DNLOAD_SYNC,
+  DFU_INT_DFU_DNBUSY,
+  DFU_INT_DFU_DNLOAD_IDLE,
+  DFU_INT_DFU_MANIFEST_SYNC,
+  DFU_INT_DFU_MANIFEST,
+  DFU_INT_DFU_MANIFEST_WAIT_RESET,
+  DFU_INT_DFU_UPLOAD_IDLE,
+  DFU_INT_DFU_ERROR,
+};
+
+enum DfuIntStatus : uint8_t {
+  DFU_INT_DFU_STATUS_OK,
+  DFU_INT_DFU_STATUS_ERR_TARGET,
+  DFU_INT_DFU_STATUS_ERR_FILE,
+  DFU_INT_DFU_STATUS_ERR_WRITE,
+  DFU_INT_DFU_STATUS_ERR_ERASE,
+  DFU_INT_DFU_STATUS_ERR_CHECK_ERASED,
+  DFU_INT_DFU_STATUS_ERR_PROG,
+  DFU_INT_DFU_STATUS_ERR_VERIFY,
+  DFU_INT_DFU_STATUS_ERR_ADDRESS,
+  DFU_INT_DFU_STATUS_ERR_NOTDONE,
+  DFU_INT_DFU_STATUS_ERR_FIRMWARE,
+  DFU_INT_DFU_STATUS_ERR_VENDOR,
+  DFU_INT_DFU_STATUS_ERR_USBR,
+  DFU_INT_DFU_STATUS_ERR_POR,
+  DFU_INT_DFU_STATUS_ERR_UNKNOWN,
+  DFU_INT_DFU_STATUS_ERR_STALLEDPKT,
+};
+
+enum DfuCommands : uint8_t {
+  DFU_CONTROLLER_SERVICER_RESID_DFU_DETACH = 0,
+  DFU_CONTROLLER_SERVICER_RESID_DFU_DNLOAD = 1,
+  DFU_CONTROLLER_SERVICER_RESID_DFU_UPLOAD = 2,
+  DFU_CONTROLLER_SERVICER_RESID_DFU_GETSTATUS = 3,
+  DFU_CONTROLLER_SERVICER_RESID_DFU_CLRSTATUS = 4,
+  DFU_CONTROLLER_SERVICER_RESID_DFU_GETSTATE = 5,
+  DFU_CONTROLLER_SERVICER_RESID_DFU_ABORT = 6,
+  DFU_CONTROLLER_SERVICER_RESID_DFU_SETALTERNATE = 64,
+  DFU_CONTROLLER_SERVICER_RESID_DFU_TRANSFERBLOCK = 65,
+  DFU_CONTROLLER_SERVICER_RESID_DFU_GETVERSION = 88,
+  DFU_CONTROLLER_SERVICER_RESID_DFU_REBOOT = 89,
+};
+
 class VoiceKit : public Component, public i2c::I2CDevice {
  public:
   void setup() override;
+  bool can_proceed() override {
+    return this->is_failed() || (this->version_read_() && (this->versions_match_() || !this->firmware_bin_is_valid_()));
+  }
   void dump_config() override;
   float get_setup_priority() const override { return setup_priority::IO; }
+  void loop() override;
 
   void set_reset_pin(GPIOPin *reset_pin) { reset_pin_ = reset_pin; }
 
-  bool dfu_get_version_();
+  void set_firmware_bin(const uint8_t *data, const uint32_t len) {
+    this->firmware_bin_ = data;
+    this->firmware_bin_length_ = len;
+  }
+  void set_firmware_version(uint8_t major, uint8_t minor, uint8_t patch) {
+    this->firmware_bin_version_major_ = major;
+    this->firmware_bin_version_minor_ = minor;
+    this->firmware_bin_version_patch_ = patch;
+  }
+
+  void start_dfu_update();
 
  protected:
+  VoiceKitUpdaterStatus dfu_update_send_block_();
+  uint32_t load_buf_(uint8_t *buf, const uint8_t max_len, const uint32_t offset);
+  bool firmware_bin_is_valid_() { return this->firmware_bin_ != nullptr && this->firmware_bin_length_; }
+  bool version_read_();
+  bool versions_match_();
+
+  bool dfu_get_status_();
+  bool dfu_get_version_();
+  bool dfu_reboot_();
+  bool dfu_set_alternate_();
+  bool dfu_check_if_ready_();
+
   GPIOPin *reset_pin_;
+
+  uint8_t dfu_state_{0};
+  uint8_t dfu_status_{0};
+  uint32_t dfu_status_next_req_delay_{0};
+
+  uint8_t const *firmware_bin_{nullptr};
+  uint32_t firmware_bin_length_{0};
+  uint8_t firmware_bin_version_major_{0};
+  uint8_t firmware_bin_version_minor_{0};
+  uint8_t firmware_bin_version_patch_{0};
+
+  uint8_t firmware_version_major_{0};
+  uint8_t firmware_version_minor_{0};
+  uint8_t firmware_version_patch_{0};
+
+  uint32_t bytes_written_{0};
+  uint32_t last_progress_{0};
+  uint32_t last_ready_{0};
+  uint32_t status_last_read_ms_{0};
+  uint32_t update_start_time_{0};
+  VoiceKitUpdaterStatus dfu_update_status_{UPDATE_OK};
 };
 
 }  // namespace voice_kit
