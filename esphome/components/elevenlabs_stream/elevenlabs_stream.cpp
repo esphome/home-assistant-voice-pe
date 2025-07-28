664f1f79145c5ec4fc9109d02611ac728ed3cd27
ffMathy <mathias.lorenzen@live.com>
2025-07-28 21:00:37 +0200

    fix: initial format

--- esphome/components/elevenlabs_stream/elevenlabs_stream.cpp
+++ esphome/components/elevenlabs_stream/elevenlabs_stream.cpp
@@ -89,23 +89,17 @@ bool ElevenLabsStream::decode_and_play_base64_audio(const char* base64_data) {
 
 // Sets the speaker's audio stream info based on the agent output format, if available.
 void ElevenLabsStream::set_speaker_stream_info_to_elevenlabs_format() {
-  // If agent_output_audio_format_ is set, configure the speaker accordingly.
-  if (!this->agent_output_audio_format_.empty()) {
-    // Example: parse format string and set speaker stream info
-    // Supported formats: "pcm_16000", "pcm_22050", "pcm_24000", "pcm_44100", "pcm_48000"
-    int sample_rate = 16000; // default
-    if (this->agent_output_audio_format_ == "pcm_16000") sample_rate = 16000;
-    else if (this->agent_output_audio_format_ == "pcm_22050") sample_rate = 22050;
-    else if (this->agent_output_audio_format_ == "pcm_24000") sample_rate = 24000;
-    else if (this->agent_output_audio_format_ == "pcm_44100") sample_rate = 44100;
-    else if (this->agent_output_audio_format_ == "pcm_48000") sample_rate = 48000;
+  // Example: parse format string and set speaker stream info
+  // Supported formats: "pcm_16000", "pcm_22050", "pcm_24000", "pcm_44100", "pcm_48000"
+  int sample_rate = 16000; // default
+  if (this->agent_output_audio_format_ == "pcm_16000") sample_rate = 16000;
+  else if (this->agent_output_audio_format_ == "pcm_22050") sample_rate = 22050;
+  else if (this->agent_output_audio_format_ == "pcm_24000") sample_rate = 24000;
+  else if (this->agent_output_audio_format_ == "pcm_44100") sample_rate = 44100;
+  else if (this->agent_output_audio_format_ == "pcm_48000") sample_rate = 48000;
 
-    esphome::audio::AudioStreamInfo info(16, 1, sample_rate);
-    this->speaker_->set_audio_stream_info(info);
-    ESP_LOGD(TAG, "SET_SPKR_INFO: Set speaker stream info from agent_output_audio_format_='%s' (sample_rate=%d)", this->agent_output_audio_format_.c_str(), sample_rate);
-  } else {
-    ESP_LOGW(TAG, "SET_SPKR_INFO: No audio format info available to set speaker stream info");
-  }
+  esphome::audio::AudioStreamInfo info(16, 1, sample_rate);
+  this->speaker_->set_audio_stream_info(info);
 }
 
 void ElevenLabsStream::setup() {
@@ -127,22 +121,24 @@ void ElevenLabsStream::setup() {
 
   this->speaker_->add_audio_output_callback([this](uint32_t _a, int64_t _b) {
     this->cancel_timeout("audio_output_callback");
-    this->set_timeout("audio_output_callback", 100, [this]() {
-      ESP_LOGD(TAG, "Speaker finished");
-      if(!this->speaker_is_active_) {
-        // If the speaker session that ended is from the wake sound, we need to switch to an ElevenLabs format.
-        this->set_speaker_stream_info_to_elevenlabs_format();
-        return;
-      }
+    this->set_timeout("audio_output_callback", 1000, [this]() {
+      this->speaker_->stop();
 
       for (auto *trigger : this->on_listening_triggers_) {
           trigger->trigger();
       }
 
+      if(!this->speaker_is_active_) {
+        // If the speaker session that ended is from the wake sound, we need to switch to an ElevenLabs format.
+        ESP_LOGD(TAG, "Speaker session ended, switching to ElevenLabs format");
+        this->set_speaker_stream_info_to_elevenlabs_format();
+        return;
+      }
+
       // If the speaker session that ended is from ElevenLabs, we need to reset the speaker stream info to the wake sound format.
+      ESP_LOGD(TAG, "Speaker session ended, resetting to wake word format");
       this->speaker_->set_audio_stream_info(this->initial_audio_stream_info_);
 
-      this->speaker_->stop();
       this->speaker_is_active_ = false;
     });
   });
@@ -371,6 +367,12 @@ void ElevenLabsStream::parse_json_message_from_buffer(const uint8_t *buffer, siz
         if (!this->initial_audio_stream_info_set_) {
           this->initial_audio_stream_info_ = this->speaker_->get_audio_stream_info();
           this->initial_audio_stream_info_set_ = true;
+
+          ESP_LOGI(TAG, 
+            "PARSE_JSON_BUF: Initial audio stream info set: %d Hz, %d channels, %d bits per sample", 
+            this->initial_audio_stream_info_->get_sample_rate(),
+            this->initial_audio_stream_info_->get_channels(),
+            this->initial_audio_stream_info_->get_bits_per_sample());
         }
         
         // Set the input audio stream info for the resampler based on ElevenLabs format
