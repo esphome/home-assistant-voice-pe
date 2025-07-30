74f5ab254cc503cd804275c8e97a69f6cb9f8163
ffMathy <mathias.lorenzen@live.com>
2025-07-30 19:57:20 +0200

    fix: don't always use com4

--- test-elevenlabs.bat
+++ test-elevenlabs.bat
@@ -1,4 +1,4 @@
 @echo off
 
 REM Run ESPHome with environment variables
-python -m esphome run --device COM4 home-assistant-voice.elevenlabs.yaml
+python -m esphome run home-assistant-voice.elevenlabs.yaml
